48522e86cbffda68b02d90d752d3118a9d439b85
Himanshu1Gupta <himanshugupta242996@gmail.com>
2024-01-31 03:27:23 +0530

    docker related changes

---
+++ web/start.sh
@@ -0,0 +1,6 @@
+/usr/sbin/nginx -g "daemon on;"
+if [[ $NEWRELIC_ENABLE = yes ]]; then
+  java -Dspring.profiles.active=$ENV_PROFILE -XX:MaxRAMPercentage=60.0 -Dnewrelic.environment=$ENV_PROFILE -javaagent:/var/newrelic/newrelic.jar -jar /app.jar
+else
+  java -Dspring.profiles.active=$ENV_PROFILE -XX:MaxRAMPercentage=60.0 -Dnewrelic.environment=$ENV_PROFILE -jar /app.jar
+fi
