850e87406e97911ef7b4ed0d970cfde308e15196
manoj-JU <nimar.manoj123@gmail.com>
2024-02-01 01:26:42 +0530

    added ProfessionalDetailsController

---
+++ web/src/main/java/com/proptiger/mortgage/web/controller/api/v0/ProfessionalDetailsController.java
@@ -0,0 +1,55 @@
+package com.proptiger.mortgage.web.controller.api.v0;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.proptiger.mortgage.web.service.api.v0.ProfessionalDetailsService;
+import com.proptiger.pyro.common.pojo.response.APIResponse;
+import com.proptiger.pyro.mortgage.request.api.v0.professionalDetails.CreateProfessionalDetailsRequestDTO;
+import com.proptiger.pyro.mortgage.request.api.v0.professionalDetails.GetProfessionalDetailsRequestDTO;
+import com.proptiger.pyro.mortgage.request.api.v0.professionalDetails.UpdateProfessionalDetailsRequestDTO;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v0")
+@RestController
+public class ProfessionalDetailsController {
+
+    @Autowired private ObjectMapper objectMapper;
+
+    @Autowired
+    private ProfessionalDetailsService professionalDetailsService;
+
+    @PostMapping(value = "/professional-details")
+    public APIResponse create(
+        @RequestBody CreateProfessionalDetailsRequestDTO requestDTO) {
+        //todo:implement this method
+        //add validations
+        return new APIResponse(professionalDetailsService.create(requestDTO));
+    }
+
+    @PutMapping(value = "/professional-details/{id}")
+    public APIResponse update(
+        @PathVariable Integer id,
+        @RequestParam(required = false)
+        @RequestBody UpdateProfessionalDetailsRequestDTO requestDTO) {
+        //todo:add validations
+        return new APIResponse(professionalDetailsService.update(id, requestDTO));
+    }
+
+    @GetMapping("/professional-details")
+    public APIResponse get(
+        @RequestParam Map<String, String> params) {
+        GetProfessionalDetailsRequestDTO getProfessionalDetailsRequestDTO =
+            objectMapper.convertValue(params, GetProfessionalDetailsRequestDTO.class);
+        //todo:add validations
+        return new APIResponse(professionalDetailsService.get(getProfessionalDetailsRequestDTO));
+    }
+
+}
---
+++ web/src/main/java/com/proptiger/mortgage/web/service/api/v0/ProfessionalDetailsService.java
@@ -0,0 +1,27 @@
+package com.proptiger.mortgage.web.service.api.v0;
+
+import com.proptiger.pyro.mortgage.request.api.v0.professionalDetails.CreateProfessionalDetailsRequestDTO;
+import com.proptiger.pyro.mortgage.request.api.v0.professionalDetails.GetProfessionalDetailsRequestDTO;
+import com.proptiger.pyro.mortgage.request.api.v0.professionalDetails.UpdateProfessionalDetailsRequestDTO;
+import com.proptiger.pyro.mortgage.response.api.v0.professionalDetails.ProfessionalDetailsResponseDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfessionalDetailsService {
+
+    public ProfessionalDetailsResponseDTO create(CreateProfessionalDetailsRequestDTO requestDTO) {
+        //todo:implement this
+        return null;
+    }
+
+    public ProfessionalDetailsResponseDTO update(Integer id, UpdateProfessionalDetailsRequestDTO requestDTO) {
+        //todo:implement this
+        return null;
+    }
+
+    public ProfessionalDetailsResponseDTO get(GetProfessionalDetailsRequestDTO getProfessionalDetailsRequestDTO) {
+        //todo:implement this
+        return null;
+    }
+
+}
